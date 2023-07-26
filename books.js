656daf0f12cb5e84144d4a55dd24b034cc0124b0
GarryAmasia <garryamasia@outlook.com>
2023-07-26 12:23:48 +1000

    all the sorting features completed,working on loading state

--- books.js
+++ books.js
@@ -1,139 +1,143 @@
 // </span> | $${book.salePrice}
 function getBooks() {
-  return [
-    {
-      id: 1,
-      title: "A million to One",
-      url: "assets/garry's books/a-million.jpeg",
-      originalPrice: 63.99,
-      salePrice: 30.99,
-      rating: 1,
-    },
-    {
-      id: 2,
-      title: "The Black Universe",
-      url: "assets/garry's books/the-black.webp",
-      originalPrice: 44.99,
-      salePrice: 19.99,
-      rating: 5,
-    },
-    {
-      id: 3,
-      title: "The Pen and The Sword",
-      url: "assets/garry's books/the-pen.jpeg",
-      originalPrice: 29.99,
-      salePrice: null,
-      rating: 4,
-    },
-    {
-      id: 4,
-      title: "Flame and Shadow",
-      url: "assets/garry's books/the-flame.webp",
-      originalPrice: 33.99,
-      salePrice: 19.99,
-      rating: 4.5,
-    },
-    {
-      id: 5,
-      title: "Brightest Star",
-      url: "assets/garry's books/emma.jpeg",
-      originalPrice: 28.99,
-      salePrice: null,
-      rating: 4.5,
-    },
-    {
-      id: 6,
-      title: "No Place Like Here",
-      url: "assets/garry's books/noplace.jpeg",
-      originalPrice: 43.99,
-      salePrice: 30.99,
-      rating: 3.5,
-    },
-    {
-      id: 7,
-      title: "Animal Science",
-      url: "assets/garry's books/animal.webp",
-      originalPrice: 28.99,
-      salePrice: 14.99,
-      rating: 4,
-    },
-    {
-      id: 8,
-      title: "The Three Month Rule",
-      url: "assets/garry's books/thethree.webp",
-      originalPrice: 13.99,
-      salePrice: 9.99,
-      rating: 4.5,
-    },
-    {
-      id: 9,
-      title: "Becoming Me",
-      url: "assets/garry's books/becoming.jpeg",
-      originalPrice: 53.99,
-      salePrice: 27.99,
-      rating: 4,
-    },
-    {
-      id: 10,
-      title: "The Covenant of Water",
-      url: "assets/garry's books/covenant.jpeg",
-      originalPrice: 29.99,
-      salePrice: 19.99,
-      rating: 4,
-    },
-    {
-      id: 11,
-      title: "The Last Man Out",
-      url: "assets/garry's books/last.jpeg",
-      originalPrice: 59.99,
-      salePrice: null,
-      rating: 4.5,
-    },
-    {
-      id: 12,
-      title: "The Silence Patient",
-      url: "assets/garry's books/silence.jpeg",
-      originalPrice: 17.99,
-      salePrice: 11.99,
-      rating: 4.5,
-    },
-    {
-      id: 13,
-      title: "Lunar Storm",
-      url: "assets/garry's books/lunar.jpeg",
-      originalPrice: 23.99,
-      salePrice: null,
-      rating: 5,
-    },
-    {
-      id: 14,
-      title: "Our Last Summer",
-      url: "assets/garry's books/last-summer.webp",
-      originalPrice: 44.99,
-      salePrice: 19.99,
-      rating: 4.5,
-    },
-    {
-      id: 15,
-      title: "The Berlin Girl",
-      url: "assets/garry's books/berlin.jpeg",
-      originalPrice: 53.99,
-      salePrice: 35.99,
-      rating: 2.5,
-    },
-    {
-      id: 16,
-      title: "Harry Potter",
-      url: "assets/garry's books/harry.jpeg",
-      originalPrice: 63.99,
-      salePrice: 30.99,
-      rating: 4.5,
-    },
-  ];
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve([
+        {
+          id: 1,
+          title: "A million to One",
+          url: "assets/garry's books/a-million.jpeg",
+          originalPrice: 63.99,
+          salePrice: 30.99,
+          rating: 1,
+        },
+        {
+          id: 2,
+          title: "The Black Universe",
+          url: "assets/garry's books/the-black.webp",
+          originalPrice: 44.99,
+          salePrice: 19.99,
+          rating: 5,
+        },
+        {
+          id: 3,
+          title: "The Pen and The Sword",
+          url: "assets/garry's books/the-pen.jpeg",
+          originalPrice: 29.99,
+          salePrice: null,
+          rating: 4,
+        },
+        {
+          id: 4,
+          title: "Flame and Shadow",
+          url: "assets/garry's books/the-flame.webp",
+          originalPrice: 33.99,
+          salePrice: 19.99,
+          rating: 4.5,
+        },
+        {
+          id: 5,
+          title: "Brightest Star",
+          url: "assets/garry's books/emma.jpeg",
+          originalPrice: 28.99,
+          salePrice: null,
+          rating: 4.5,
+        },
+        {
+          id: 6,
+          title: "No Place Like Here",
+          url: "assets/garry's books/noplace.jpeg",
+          originalPrice: 43.99,
+          salePrice: 30.99,
+          rating: 3.5,
+        },
+        {
+          id: 7,
+          title: "Animal Science",
+          url: "assets/garry's books/animal.webp",
+          originalPrice: 28.99,
+          salePrice: 14.99,
+          rating: 4,
+        },
+        {
+          id: 8,
+          title: "The Three Month Rule",
+          url: "assets/garry's books/thethree.webp",
+          originalPrice: 13.99,
+          salePrice: 9.99,
+          rating: 4.5,
+        },
+        {
+          id: 9,
+          title: "Becoming Me",
+          url: "assets/garry's books/becoming.jpeg",
+          originalPrice: 53.99,
+          salePrice: 27.99,
+          rating: 4,
+        },
+        {
+          id: 10,
+          title: "The Covenant of Water",
+          url: "assets/garry's books/covenant.jpeg",
+          originalPrice: 29.99,
+          salePrice: 19.99,
+          rating: 4,
+        },
+        {
+          id: 11,
+          title: "The Last Man Out",
+          url: "assets/garry's books/last.jpeg",
+          originalPrice: 59.99,
+          salePrice: null,
+          rating: 4.5,
+        },
+        {
+          id: 12,
+          title: "The Silence Patient",
+          url: "assets/garry's books/silence.jpeg",
+          originalPrice: 17.99,
+          salePrice: 11.99,
+          rating: 4.5,
+        },
+        {
+          id: 13,
+          title: "Lunar Storm",
+          url: "assets/garry's books/lunar.jpeg",
+          originalPrice: 23.99,
+          salePrice: null,
+          rating: 5,
+        },
+        {
+          id: 14,
+          title: "Our Last Summer",
+          url: "assets/garry's books/last-summer.webp",
+          originalPrice: 44.99,
+          salePrice: 19.99,
+          rating: 4.5,
+        },
+        {
+          id: 15,
+          title: "The Berlin Girl",
+          url: "assets/garry's books/berlin.jpeg",
+          originalPrice: 53.99,
+          salePrice: 35.99,
+          rating: 2.5,
+        },
+        {
+          id: 16,
+          title: "Harry Potter",
+          url: "assets/garry's books/harry.jpeg",
+          originalPrice: 63.99,
+          salePrice: 30.99,
+          rating: 4.5,
+        },
+      ]);
+    }, 2000);
+  });
 }
 
-const renderBooks = (filter) => {
-  const booksList = getBooks();
+const renderBooks = async (filter) => {
+  const booksList = await getBooks();
   const booksWrapper = document.querySelector(".books");
   //   console.log(booksList);
   // booksList.map((book) => {
@@ -158,11 +162,18 @@ const renderBooks = (filter) => {
   //                 </div>
   //               </div>`);
   // });
+  console.log(booksList);
+
   if (filter === "low_to_high") {
-    booksList.sort((a, b) => a.originalPrice - b.originalPrice);
+    booksList.sort(
+      (a, b) =>
+        (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice)
+    );
   } else if (filter === "high_to_low") {
     booksList.sort((a, b) => {
-      return b.originalPrice - a.originalPrice;
+      return (
+        (b.salePrice || b.originalPrice) - (a.salePrice || a.originalPrice)
+      );
     });
   } else if (filter === "rating") {
     booksList.sort((a, b) => b.rating - a.rating);
